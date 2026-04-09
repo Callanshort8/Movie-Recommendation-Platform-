2991ffe1d90c05cee290881fd0f8a1be7df56df2
MackydLow <2636498@dundee.ac.uk>
2026-04-09 19:23:08 +0000

    starer the frontedn and backend connection

---
+++ api.js
@@ -0,0 +1,32 @@
+//shared by all pages
+const API = 'http://localhost:8000';
+
+function getToken() {
+    return localStorage.getItem('token');
+}
+
+function authHeaders()n{
+    return {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ${getToken()}'
+    };
+}
+
+function requireAuth() {
+    if (!getToken()) window.location.href = 'index.html';
+}
+
+async function apiGet(path) {
+    const res = await fetch(API + path, { headers: authHeaders()});
+    if (res.status === 401) {localStorage.removeItem('token'); window.location.href = 'index.html';}
+    return res.json();
+}
+
+async function apiPost(path, body = {}) {
+    const res await fetch(API + path, {
+        method: 'POST',
+        headers: authHeaders(),
+        body: JSON.stringify(body)
+    });
+    return res.json();
+}
