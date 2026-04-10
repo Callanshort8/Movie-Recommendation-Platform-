471df82ec071bbc71baf6db67c7a68515003df61
MackydLow <2636498@dundee.ac.uk>
2026-04-10 18:58:50 +0000

    save

--- database.py
+++ database.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 dbPool = pooling.MySQLConnectionPool(
-    poolName = "moviePool",
+    pool_name = "moviePool",
     pool_size = 5,
     host = os.getenv("DB_HOST"),
     user = os.getenv("DB_USER"),
